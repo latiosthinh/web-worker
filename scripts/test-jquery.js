c421ee1f05b908d63f82575f9a51dd183006c19b
Thinh.Nguyen <thinh.nguyen@niteco.se>
2024-10-29 13:35:20 +0700

    feat: update a piece of jquery code

--- scripts/test-jquery.js
+++ scripts/test-jquery.js
@@ -10,4 +10,10 @@ pubsub.on('jquery-loaded', () => {
 		setTimeout(() => {
 			normalFunction()
 		}, 1000);
+
+		$(".accordion-title").click(function(){
+			$(this).parent(".accordion-item").find(".accordion-content").slideToggle();
+			$(this).parent(".accordion-item").prevAll(".accordion-item").find(".accordion-content").slideUp();
+			$(this).parent(".accordion-item").nextAll(".accordion-item").find(".accordion-content").slideUp();
+		});
 })
